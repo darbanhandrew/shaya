db828ff5138ba4d86a27a3cc59801c020f524da6
Mohammad <darbanhandrew@gmail.com>
2021-02-27 17:37:18 +0330

    changed to staticfiles

--- taha_back/taha/settings.py
+++ taha_back/taha/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # STATICFILES_DIRS=[os.path.join(BASE_DIR, "statics")]
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 GRAPHENE = {
