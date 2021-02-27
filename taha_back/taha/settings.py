910f37756ac153518735a9ccef279c5668650942
Mohammad <darbanhandrew@gmail.com>
2021-02-27 17:34:13 +0330

    removed static

--- taha_back/taha/settings.py
+++ taha_back/taha/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS=[os.path.join(BASE_DIR, "statics")]
+# STATICFILES_DIRS=[os.path.join(BASE_DIR, "statics")]
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/mediafiles/"
